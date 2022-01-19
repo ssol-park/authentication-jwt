e2cdbd2ab250544278cb136b1c1c14c2cbff8d37
ssol-park <ksttlov.naver@hanmail.net>
2022-01-19 22:57:01 +0900

    Feat: added Entity, set jwt TokenProvider

---
+++ src/main/java/com/authentication/jwt/config/TokenProvider.java
@@ -0,0 +1,97 @@
+package com.authentication.jwt.config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class TokenProvider implements InitializingBean {
+
+    private static final String AUTHORITIES_KET = "auth";
+
+    private final String secret;
+
+    private final long tokenValidityInMilliSeconds;
+
+    private Key key;
+
+    public TokenProvider(
+            @Value("{${jwt.secret}") String secret,
+            @Value("{${jwt.token-validity-in-seconds}") long tokenValidityInMilliSeconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliSeconds = tokenValidityInMilliSeconds;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        byte[] ketBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(ketBytes);
+    }
+
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliSeconds);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KET, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KET).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.error("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+}
--- src/main/java/com/authentication/jwt/security/SecurityConfig.java
+++ src/main/java/com/authentication/jwt/security/SecurityConfig.java
@@ -1,11 +1,19 @@
-package com.authentication.jwt.config;
+package com.authentication.jwt.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/favicon.ico");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
