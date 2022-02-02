973cc956b081cea5af1b4a730066c73bae61da5a
ssol-park <ksttlov.naver@hanmail.net>
2022-02-02 14:31:19 +0900

    Feat: added repository, login

---
+++ src/main/java/com/authentication/jwt/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package com.authentication.jwt.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+
+}
