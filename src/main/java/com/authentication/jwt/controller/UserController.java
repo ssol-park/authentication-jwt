0b42ebc0927478df7adee4bb5e2337d8c3286c1b
ssol-park <ksttlov.naver@hanmail.net>
2022-02-02 15:00:28 +0900

    Feat: added signup

---
+++ src/main/java/com/authentication/jwt/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.authentication.jwt.controller;
+
+import com.authentication.jwt.dto.UserDto;
+import com.authentication.jwt.entity.User;
+import com.authentication.jwt.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@Valid @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<User> getMyUserInfo() {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<User> getUserInfo(@PathVariable String  username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+    }
+
+}
